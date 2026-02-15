a5f3523caaaf80a3cf0ddb45a09ebc432100c8f0
Elijah <elijah.selvaratnam@gmail.com>
2026-02-15 08:51:04 +0000

    Final Project

---
+++ Leap_Year.py
@@ -0,0 +1,24 @@
+def Leap_Year_Check(year, revenue):
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+             if revenue > 1000000 :
+                print("Platinum Year")
+             if year % 400 == 0:
+                if revenue < 500000:
+                    print("Redistribution Year")
+    else:
+        y = str(year)
+        r = str(revenue)
+        last_2_digits_y = y[len(y)-2] + y[len(y)-1]
+        last_2_digits_r = r[len(r)-2] + r[len(r)-1]
+        if int(last_2_digits_y) != 0:
+            if int(last_2_digits_r) % int(last_2_digits_y) == 0:
+                print("Secular Anomaly")
+print("Standard Cycle")
+
+
+
+
+Leap_Year_Check(2000, 400000)
+Leap_Year_Check(2024, 1200000)
+Leap_Year_Check(1925, 50)
+Leap_Year_Check(1900, 1500000)
